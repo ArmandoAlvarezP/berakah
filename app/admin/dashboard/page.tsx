912310f4edcad4a16c08457c2158ec6d2f0ed435
Armando AP <armando.alvarez.gkm@hotmail.com>
2026-03-21 23:06:46 -0600

    Se añaden posibles iconos para las acciones en tabla de directorio de alumnos

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,3 +1,6 @@
+import { FaPlus, FaPlusCircle } from "react-icons/fa";
+import { IoTrashOutline } from "react-icons/io5";
+import { MdEdit } from "react-icons/md";
 
 export default function AdminPage() {
     return (
@@ -22,21 +25,94 @@ export default function AdminPage() {
                     </thead>
                     <tbody className="block md:table-row-group">
                         <tr className="border border-grey-500 md:border-none block md:table-row">
-                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3 md:hidden font-bold mr-2 sm:mr-0">Alumno</span>Julián Barrera</td>
+                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3 md:hidden font-bold mr-2 sm:mr-0">Alumno</span>
+                                <div className="flex flex-col">
+                                    <span className="text-center">Julián Barrera</span>
+                                    <div className="flex flex-row justify-center mt-5">
+                                        <MdEdit size={20} className="m-2 text-green-400 hover:text-green-600 cursor pointer"/>
+                                        <IoTrashOutline size={20} className="m-2 text-red-400 hover:text-red-600 cursor-pointer"/>
+                                    </div>
+                                </div>
+                            </td>
                             <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3 md:hidden font-bold mr-2 sm:mr-0">CURP</span>AAPA940623HMNLR04</td>
-                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3 md:hidden font-bold mr-2 sm:mr-0">Tipo de Certificación</span>Certificado de Especialización Profesional</td>
-                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3 md:hidden font-bold mr-2 sm:mr-0">Especialidad</span>Mecatrónica</td>
-                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3 md:hidden font-bold mr-2 sm:mr-0">Institución Emisora</span>Berakah Consultoria</td>
-                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3 md:hidden font-bold mr-2 sm:mr-0">Institución Autorizada</span>Berakah Consultoria</td>
+                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3 md:hidden font-bold mr-2 sm:mr-0">Tipo de Certificación</span>
+                            <div className="flex flex-col">
+                                    <ol>
+                                        <li className="ml-5 
+                                        list-decimal">Certificado de especialidad SEP</li>
+                                        <li className="ml-5 
+                                        list-decimal">Certificado de Especialización Profesional</li>
+                                        <li className="ml-5 
+                                        list-decimal">Instructor Capacitador SEP</li>
+                                    </ol>
+                                    <FaPlus size={20} className="ml-25 text-green-400 hover:text-green-600 cursor-pointer" />
+                                </div>
+                            </td>
+                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3 md:hidden font-bold mr-2 sm:mr-0">Especialidad</span>
+                            <div className="flex flex-row">
+                                    <ol>
+                                        <li className="ml-5 
+                                        list-decimal">Mecatrónica</li>
+                                        <li className="ml-5 
+                                        list-decimal">Mecatrónica</li>
+                                        <li className="ml-5 
+                                        list-decimal">Mecatrónica</li>
+                                    </ol>
+                                </div>
+                            </td>
+                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3 md:hidden font-bold mr-2 sm:mr-0">Institución Emisora</span>
+                                <div className="flex flex-row">
+                                    <ol>
+                                        <li className="ml-5 
+                                        list-decimal">Berakah Consultoria</li>
+                                        <li className="ml-5 
+                                        list-decimal">Berakah Consultoria</li>
+                                        <li className="ml-5 
+                                        list-decimal">Berakah Consultoria</li>
+                                    </ol>
+                                </div>
+                            </td>
+                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3 md:hidden font-bold mr-2 sm:mr-0">Institución Autorizada</span>
+                                <div className="flex flex-row">
+                                    <ol>
+                                        <li className="ml-5 
+                                        list-decimal">Berakah Consultoria</li>
+                                        <li className="ml-5 
+                                        list-decimal">Berakah Consultoria</li>
+                                        <li className="ml-5 
+                                        list-decimal">Berakah Consultoria</li>
+                                    </ol>
+                                </div>
+                            </td>
                             <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3 md:hidden font-bold mr-2 sm:mr-0">Fecha de Emisión</span>
-                            21/03/2026</td>
-                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3 md:hidden font-bold mr-2 sm:mr-0">Folio</span>1234</td>
+                                <div className="flex flex-row">
+                                    <ol>
+                                        <li className="ml-5 
+                                        list-decimal">19/03/2026</li>
+                                        <li className="ml-5 
+                                        list-decimal">19/03/2026</li>
+                                        <li className="ml-5 
+                                        list-decimal">19/03/2026</li>
+                                    </ol>
+                                </div>
+                            </td>
+                            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                <span className="inline-block w-1/3 md:hidden font-bold mr-2 sm:mr-0">Folio</span>
+                                <div className="flex flex-row">
+                                    <ol>
+                                        <li className="ml-5 
+                                        list-decimal">1234</li>
+                                        <li className="ml-5   list-decimal">2345</li>
+                                        <li className="ml-5   list-decimal">6789</li>
+                                    </ol>
+                                </div>
+                            </td>
                             <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3 md:hidden font-bold mr-2 sm:mr-0">Acciones</span>
                                 <div className="flex flex-row">
                                     <ul>
-                                        <li className="text-green-500 underline cursor-pointer">Añadir Especialidad</li>
-                                        <li className="text-blue-500 underline cursor-pointer">Editar Alumno</li>
-                                        <li className="text-red-500 underline cursor-pointer">Eliminar Alumno</li>
+                                        <li className="ml-5 text-green-500 underline cursor-pointer">Añadir Especialidad</li>
+                                        <li className="ml-5 text-blue-500 underline cursor-pointer">Editar Alumno</li>
+                                        <li className="ml-5 text-red-500 underline cursor-pointer">Eliminar Alumno</li>
                                     </ul>
                                 </div>
                             </td>
