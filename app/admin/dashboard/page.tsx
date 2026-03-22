ead514b34e0e387c432fa973f5bbee4d5d0bf6ca
Armando AP <armando.alvarez.gkm@hotmail.com>
2026-03-22 17:31:11 -0600

    Se ajustan estilos del modal agregar alumnos

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -20,28 +20,28 @@ export default function AdminPage() {
                         <h1 className="text-[#00795D] text-3xl text-center font-bold tracking-normal leading-tight mb-4 uppercase">
                             Agregar Alumno
                         </h1>
-                        <input id="nombre" name="nombre" type="text" className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-[#00795D] font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded-3xl border" placeholder="NOMBRE COMPLETO" />
+                        <input id="nombre" name="nombre" type="text" className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-[#111817] font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded-3xl border" placeholder="NOMBRE COMPLETO" />
                         <input id="curp" name="curp" type="text" className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-[#00795D] font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded-3xl border" placeholder="CURP" />
-                        <select id="tipo" name="tipo" className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-[#00795D] font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded-3xl border">
-                            <option value="" className="text-gray-600 uppercase">TIPO DE CERTIFICACIÓN</option>
-                            <option value="1" className="uppercase">
+                        <select id="tipo" name="tipo" className="mb-5 mt-2 text-gray-400 focus:outline-none focus:border focus:border-[#00795D] w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded-3xl border uppercase">
+                            <option value="">TIPO DE CERTIFICACIÓN</option>
+                            <option value="1">
                                 Diploma Certificado de especialidad SEP
                             </option>
-                            <option value="2" className="uppercase">
+                            <option value="2">
                                 Diploma Certificado de especialidad
                             </option>
-                            <option value="3" className="uppercase">
+                            <option value="3">
                                 Instructor Capacitador SEP
                             </option>
-                            <option value="4" className="uppercase">
+                            <option value="4">
                                 Certificado de Acreditación BCPP
                             </option>
-                            <option value="5" className="uppercase">
+                            <option value="5">
                                 Certificado de Especialización Profesional
                             </option>
                         </select>
-                        <label>FECHA DE EMISIÓN</label>
-                        <input id="fecha" name="fecha" type="date" className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-[#00795D] font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded-3xl border"/>
+                        <label className='text-gray-400' >FECHA DE EMISIÓN</label>
+                        <input id="fecha" name="fecha" type="date" className="mb-5 mt-2 text-gray-400 focus:outline-none focus:border focus:border-[#00795D] font-normal w-full h-10 flex items-center pl-2 text-md border-gray-300 rounded-3xl border"/>
                         <input id="especialidad" name="especialidad" type="text" className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-[#00795D] font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded-3xl border" placeholder="ESPECIALIDAD" />
                         <input id="instEm" name="instEm" type="text" className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-[#00795D] font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded-3xl border" placeholder="INSTITUCIÓN EMISORA" />
                         <input id="instAut" name="instAut" type="text" className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-[#00795D] font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded-3xl border" placeholder="INSTITUCIÓN AUTORIZADA" />
