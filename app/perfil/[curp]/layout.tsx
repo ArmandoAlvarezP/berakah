28797df40f2b5ea2d2ea082c55487768c71fe1de
Armando AP <armando.alvarez.gkm@hotmail.com>
2026-03-26 13:36:39 -0600

    correción de nombre del alumno en el navbar

--- app/perfil/[curp]/layout.tsx
+++ app/perfil/[curp]/layout.tsx
@@ -1,10 +1,27 @@
+import { prisma } from '@/app/lib/prisma';
 import { NavBar, Footer } from '@/components';
 
-export default function PerfilLayout({children}: 
-    {children: React.ReactNode;}) {
+interface Props {
+    children: React.ReactNode;
+    params: Promise<string>
+}
+
+
+export default async function PerfilLayout({children, params}: Props) {
+        
+
+    const curp = await params;
+    // LLamar aquí el server action para obterner el curp 
+
+    const nombre = await prisma.alumno.findFirst({
+        where: {
+            curp: Object.values(curp).toString()
+        }
+    })
+    
         return (
             <div className='flex flex-col min-h-screen'>
-                <NavBar title='Historial de Certificaciones' welcome='Bienvenido nombre del alumno' />
+                <NavBar title='Historial de Certificaciones' welcome={`Bienvenido ${nombre?.nombre}`} />
                 <div className='grow'>
                     {children}
                 </div>
